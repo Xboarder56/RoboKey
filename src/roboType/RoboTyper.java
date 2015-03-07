d5d98af8d82b0286620f36fb3c5331d74494530c
xboarder56 <garrettbeasley@icloud.com>
2015-03-06 22:09:28 -0800

    Intial commit of the program.

---
+++ src/roboType/RoboTyper.java
@@ -0,0 +1,90 @@
+/**
+ * @author sn0w
+ */
+
+package roboType;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RoboTyper
+{
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args)
+	{
+		boolean roboRun = true;
+		
+        Random random = new Random();
+        
+        @SuppressWarnings("rawtypes")
+		ArrayList keys = new ArrayList();
+        keys.add(KeyEvent.VK_0);
+        keys.add(KeyEvent.VK_1);
+        keys.add(KeyEvent.VK_2);
+        keys.add(KeyEvent.VK_3);
+        keys.add(KeyEvent.VK_4);
+        keys.add(KeyEvent.VK_5);
+        keys.add(KeyEvent.VK_6);
+        keys.add(KeyEvent.VK_7);
+        keys.add(KeyEvent.VK_8);
+        keys.add(KeyEvent.VK_9);
+        keys.add(KeyEvent.VK_A);
+        keys.add(KeyEvent.VK_B);
+        keys.add(KeyEvent.VK_C);
+        keys.add(KeyEvent.VK_D);
+        keys.add(KeyEvent.VK_E);
+        keys.add(KeyEvent.VK_F);
+        keys.add(KeyEvent.VK_G);
+        keys.add(KeyEvent.VK_H);
+        keys.add(KeyEvent.VK_I);
+        keys.add(KeyEvent.VK_J);
+        keys.add(KeyEvent.VK_K);
+        keys.add(KeyEvent.VK_L);
+        keys.add(KeyEvent.VK_M);
+        keys.add(KeyEvent.VK_N);
+        keys.add(KeyEvent.VK_O);
+        keys.add(KeyEvent.VK_P);
+        keys.add(KeyEvent.VK_Q);
+        keys.add(KeyEvent.VK_R);
+        keys.add(KeyEvent.VK_S);
+        keys.add(KeyEvent.VK_T);
+        keys.add(KeyEvent.VK_U);
+        keys.add(KeyEvent.VK_V);
+        keys.add(KeyEvent.VK_W);
+        keys.add(KeyEvent.VK_X);
+        keys.add(KeyEvent.VK_Y);
+        keys.add(KeyEvent.VK_Z);
+        keys.add(KeyEvent.VK_ENTER);
+        keys.add(KeyEvent.VK_SPACE);
+        keys.add(KeyEvent.VK_BACK_SPACE);
+        
+        
+        while(roboRun)
+        {
+		   	try 
+			{
+		   	
+		    Robot robot = new Robot();
+		    int delay = random.nextInt(10000)+5500;
+		    Thread.sleep(delay);
+		    
+		    robot.mousePress(InputEvent.BUTTON1_MASK);
+		    robot.mouseRelease(InputEvent.BUTTON1_MASK);
+		
+		    int randomKey = random.nextInt(keys.size());
+		    robot.keyPress((int) keys.get(randomKey));
+		    robot.keyRelease((int) keys.get(randomKey));
+		    
+		    
+			}
+			catch (AWTException | InterruptedException e) 
+			{
+		        e.printStackTrace();
+			}
+        }
+	}
+}
